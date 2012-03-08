c7ce54984f107149b0cfa7e0126aa663ac406a3b
Robert Stewart <bstewart.ny@gmail.com>
2012-03-07 21:49:24 -0500

    Refactoring.

--- feeds/feeds.py
+++ feeds/feeds.py
@@ -15,19 +15,21 @@ INDEX_URL='http://localhost:8983/solr'
 
 index=solr.Solr(INDEX_URL)
 
-Newlines = re.compile(r'[\r\n]\s+')
+invalid_entities=['tweet text',
+    'white house','continue reading','written by','united states',
+    'new york','new york city','new york times','washington post',
+    'raw story','fox news']
 
 def extract_entity_names(t):
-    entity_names = []
-    
-    if hasattr(t, 'node') and t.node:
-        if t.node == 'NE':
-            entity_names.append(' '.join([child[0] for child in t]))
-        else:
-            for child in t:
-                entity_names.extend(extract_entity_names(child))
-                
-    return entity_names
+  entity_names = []
+  if hasattr(t, 'node') and t.node:
+      if t.node == 'NE':
+          entity_names.append(' '.join([child[0] for child in t]))
+      else:
+          for child in t:
+              entity_names.extend(extract_entity_names(child))
+              
+  return entity_names
  
 def is_substr(s,a):
   s=s.lower()
@@ -41,24 +43,17 @@ def is_substr(s,a):
           return True
   return False
 
-invalid_entities=['continue reading','written by','united states','new york','new york city','new york times','washington post','raw story','fox news']
-
 
 def unique_entities(a):
   u=[]
   a=list(set(a))
-
   # remove any single-word entities (too ambiguous in most cases)
   a=[s for s in a if len(s.split())>1]
   # remove any entities more than 3 words
   a=[s for s in a if len(s.split())<4]
-  
   a.sort(lambda x,y:cmp(len(y),len(x)))
-  
   a=[x for x in a if not is_substr(x,a)]
-
   a=[s for s in a if not s.lower() in invalid_entities]
-
   return a
 
 def get_entities(text):
@@ -74,220 +69,214 @@ def get_entities(text):
   return unique_entities(entity_names)
 
 def get_text_lynx(data):
-    try:
-        return subprocess.Popen(['/usr/local/bin/lynx', 
-                      '-assume-charset=UTF-8', 
-                      '-display-charset=UTF-8', 
-                      '-dump', 
-                      '-stdin'], 
-                      stdin=subprocess.PIPE, 
-                      stdout=subprocess.PIPE).communicate(input=data.encode('utf-8'))[0].decode('utf-8')
-    except:
-          print 'error getting text from lynx'
-    return ''
+  try:
+      return subprocess.Popen(['/usr/local/bin/lynx', 
+                    '-assume-charset=UTF-8', 
+                    '-display-charset=UTF-8', 
+                    '-dump', 
+                    '-stdin'], 
+                    stdin=subprocess.PIPE, 
+                    stdout=subprocess.PIPE).communicate(input=data.encode('utf-8'))[0].decode('utf-8')
+  except:
+        print 'error getting text from lynx'
+  return ''
 
 def get_page_text(url):
-	txt=''
-        data=None
-	try:
-		# given a url, get page content
-		data = urllib2.urlopen(url).read()
-		
-                if not data is None:
-		  return strip_html_tags(Document(data).summary())
-	except:
-		print 'error getting text from url: '+url
-                if not data is None:
-                  return get_text_lynx(data)
-	return txt
+  txt=''
+  data=None
+  try:
+    data = urllib2.urlopen(url).read()
+    if not data is None:
+      return strip_html_tags(Document(data).summary())
+  except:
+    print 'error getting text from url: '+url
+    if not data is None:
+      return get_text_lynx(data)
+  return txt
 
 def create_id_slug(s):
-	# strip out non-URL and non-Lucene friendly stuff...
-	bad_chars="'+-&|!(){}[]^\"~*?:\\"
-	s=s.strip()
-	s=s.replace('http://','')
-	s=s.replace('https://','')
-	for c in bad_chars:
-		s=s.replace(c,'_')
-	
-	while '__' in s:
-		s=s.replace('__','_')
-	
-	return s	
+  # strip out non-URL and non-Lucene friendly stuff...
+  bad_chars="'+-&|!(){}[]^\"~*?:\\"
+  s=s.strip().replace('http://','').replace('https://','')
+  for c in bad_chars:
+    s=s.replace(c,'_')
+  while '__' in s:
+    s=s.replace('__','_')
+  return s	
 
 def strip_html_tags(html):
-	# just get appended text elements from HTML
-        try:
-          text="".join(BeautifulSoup(html,convertEntities=BeautifulSoup.HTML_ENTITIES).findAll(text=True))
-	  return text
-        except:
-          print 'Failed to strip html tags...'
-          return html
+  # just get appended text elements from HTML
+  try:
+    text="".join(BeautifulSoup(html,convertEntities=BeautifulSoup.HTML_ENTITIES).findAll(text=True))
+    return text
+  except:
+    print 'Failed to strip html tags...'
+    return html
 
 def get_attribute(item,names):
-	for name in names:
-		if name in item:
-			return item[name]
-	return None
+  for name in names:
+    if name in item:
+      return item[name]
+  return None
 
 utc=pytz.utc
 
 def get_item_date(item):
-	#return None
-	d=get_attribute(item,['published_parsed','updated_parsed','created_parsed','date_parsed'])
-	if not d is None:
-		try:
-                  print 'got date attribute: '
-                  print d
-                  dt=datetime.datetime.fromtimestamp(time.mktime(d))
-                  dt=dt.replace(tzinfo=utc)
-                  #dt=dt.astimezone(utc)
+  d=get_attribute(item,['published_parsed','updated_parsed','created_parsed','date_parsed'])
+  if not d is None:
+    try:
+      return datetime.datetime.fromtimestamp(time.mktime(d)).replace(tzinfo=utc)
+    except:
+      return None
+  return None
 
-		  print 'parsed date:'
-                  print dt
-                  return dt
-                except:
-		  return None
-	return None	
+max_summary_len=400
+min_sentence_len=20
+max_sentence_len=300
+min_clean_words=6
+min_clean_words_ratio = 0.8
+min_word_len=2
+max_word_len=25
+
+def clean_sentence(sent):
+  # remove date line
+  # TODO: also look for unicode 'long dash' here...
+  m=re.match('^.+--\s',sent)
+  if m is not None:
+    sent=sent[m.end():]
+  return sent
+
+def is_clean_word(word):
+  if len(word)>max_word_len:
+    return False
+  if len(word)<min_word_len:
+    return False
+  if re.match('^[a-z]+$',word) is None and re.match('^[A-Z][a-z]+$',word) is None:
+    return False
+  return True
+
+def is_clean_sentence(sent):
+  if len(sent) < min_sentence_len:
+    return False
+  if len(sent) > max_sentence_len:
+    return False
+  if not sent[0].upper()==sent[0]:
+    return False
+  if not sent[-1]=='.':
+    return False
+  words=nltk.word_tokenize(sent)
+  num_clean_words=len(filter(is_clean_word,words))
+  if num_clean_words < min_clean_words:
+    return False
+  if float(num_clean_words) / float(len(words)) < min_clean_words_ratio:
+    return False
+  return True
+
+def clean_summary(summary):
+  # get one or two very clean sentences from the text
+  if summary is None:
+    return None
+  if len(summary)==0:
+    return None
+  clean_sentences=[]
+  total_len=0
+  for sentence in filter(is_clean_sentence,map(clean_sentence,nltk.sent_tokenize(summary))):
+    if total_len+len(sentence) > max_summary_len and len(clean_sentences)>0:
+      return ' '.join(clean_sentences)
+    else:
+      clean_sentences.append(sentence)
+      total_len=total_len+len(sentence)
+  
+  if len(clean_sentences)>0:
+    return ' '.join(clean_sentences)
+  else:
+    if len(summary)>200:
+      return summary[:200]+'...'
+    else:
+      return summary
 
 def analyze_feed_item(item):
 	
-	# strip HTML tags from summary/description
-	summary=get_attribute(item,['summary','description'])
-	
-	if not summary is None:
-		summary=strip_html_tags(summary)
-		item["summary"]=summary
-	
-
-	link=item['link']
-	if len(link)>0:
-		print 'Get extracted text: '+link
-		text=get_page_text(link)
-		if len(text)>len(summary):
-			item["summary"]=text
-                
-        text=item['summary']
-
-        if len(text)>0:
-          print 'get entities...'
-          entity_names=get_entities(text)
-          item['entity']=entity_names 
-
-
-        clusterid=get_cluster(item)
-        if not clusterid is None:
-          if len(clusterid)>0:
-            item['clusterid']=clusterid
-
-	return item
-
-def compute_similarity(a,b):
-  return 0
-
-def find_similar(query):
-  return []
-
-def get_cluster(item):
-  min_sim=20.0
-
-  # get features from item
+  # strip HTML tags from summary/description
+  summary=get_attribute(item,['summary','description'])
   
-  # form query using features
-  query='+contents:('
-  text=item['title'] + ' ' + item['summary']
-  for word in text.split():
-    query+=' '+word+' '
-  query+=')'
+  if not summary is None:
+    summary=strip_html_tags(summary)
+    item["summary"]=clean_summary(summary)
 
-  # find similar items in index
-  similar_items=find_similar(query)
-
-  max_sim=0
-  max_sim_item=None
-  # compute similarity to items
-
-  for sim_item in similar_items:
-    sim=compute_similarity(item,sim_item)
-    if sim >= min_sim and sim > max_sim:
-      max_sim=sim
-      max_sim_item=sim_item
-  
-  # pick most similar item and get that cluster id
-  if max_sim>=min_sim and max_sim_item is not None:
-    return max_sim_item['clusterid']
+  link=item['link']
+  text=summary
+  if len(link)>0:
+    print 'Get extracted text: '+link
+    text=get_page_text(link)
+    if text is None or len(summary)>len(text):
+      text=summary
+    item["body"]=text
   else:
-    return None
+    item["body"]=summary
 
-def get_solr_doc(item):
-	doc={}
-	doc["title"]=item["title"]
-	doc["summary"]=item["summary"]
-	d=get_item_date(item)
-	if not d is None:
-		doc["date"]=d
-	if 'author' in item:
-		doc['author']=item['author']
-	doc['feed']=item['feed']
-	doc['feedlink']=item['feedlink']
-	#doc["date"]=item["date"]
-	doc["wing"]=item["wing"]
-	doc["link"]=item["link"]
-        if 'entity' in item:
-            doc['entity']=item['entity']
+  if item["summary"] is None or len(item["summary"])==0:
+    item["summary"]=clean_summary(text)
+    
+  text = item["title"] + " "+text
+  
+  item['entity']=get_entities(text)
 
-	doc["id"]=create_id_slug(get_attribute(doc,["link","title"]))
+  return item
 
-	return doc
+def copy_attribute(source,dest,name):
+  if name in source:
+    if not name in dest:
+      dest[name]=source[name]
+
+def copy_attributes(source,dest,names):
+  for name in names:
+    copy_attribute(source,dest,name)
+
+def create_solr_doc(item):
+  doc={}
+  copy_attributes(item,doc,['title','summary','body','author','feed','feedlink','wing','link','entity'])
+  d=get_item_date(item)
+  if d is not None:
+    doc["date"]=d
+  doc["id"]=create_id_slug(get_attribute(doc,["link","title"]))
+  return doc
 
 def index_doc(doc):
-	# send document to solr
-	retries=0
-       
-	while retries<3:
-		try:
-			index.add(doc,commit=False)
-			break
-		except Exception as e:
-                        print e
-			print 'indexing failed...'
-			time.sleep(1)
-			retries=retries+1
-			if retries<3:
-				print 'retrying...'
-			else:
-				print 'retries failed.'
+  index.add(doc,commit=False)
 
+def index_error_queue():
+  for doc in queue:
+    index.add(doc,commit=False)
 
 def index_commit():
-	index.commit()
+  index.commit()
+
+def process_item(feed,item):
+  copy_attributes(feed,item,['wing','feed','name','rss','link','author'])
+  index_doc(create_solr_doc(analyze_feed_item(item)))
+
+def process_feed(feed):
+  print 'processing '+feed['wing'] +' wing feed: '+feed['feed'] +'...'
+  rss=feedparser.parse(feed['rss'])
+  count=0
+  for item in rss["items"]:
+    process_item(feed,item)
+    count=count+1
+    if count % 10 ==0:
+      index_commit()
+  print 'processed '+str(count)+' items from feed.'
+  return count
 
 def process_feeds(feeds):
-	total=0
-	print 'processing '+str(len(feeds))+' feeds...'
-	for feed in feeds:
-		print 'processing '+feed['wing'] +' wing feed: '+feed['name'] +'...'
-		rss=feedparser.parse(feed['rss'])
-		count=0
-		for item in rss["items"]:
-			item["wing"]=feed['wing']
-			item['feed']=feed['name']
-			item['feedlink']=feed['link']
-			if 'author' in feed:
-				item['author']=feed['author']
-			item=analyze_feed_item(item)
-			doc=get_solr_doc(item)
-			index_doc(doc)
-			count=count+1
-			if count % 10 ==0:
-				index_commit()
-		print 'processed '+str(count)+' items from feed.'
-		total=total+count
-	
-	index_commit()
-	print 'processed '+str(total)+' total items.'
-
+  total=0
+  print 'processing '+str(len(feeds))+' feeds...'
+  for feed in feeds:
+    total=total+process_feed(feed)
+  
+  index_commit()
+  print 'processed '+str(total)+' total items.'
 
 if __name__ == "__main__":
-	process_feeds(feeddefs.feeds)
+  process_feeds(feeddefs.feeds)
 
--- web/app.py
+++ web/app.py
@@ -1,5 +1,6 @@
 import tornado.ioloop
 import tornado.web
+import feeds
 import solr
 import os
 from tornado.template import Template
@@ -9,158 +10,97 @@ import operator
 SOLR_URL='http://localhost:8983/solr'
 
 class SearchHandler(tornado.web.RequestHandler):
-	
-	def get(self):
-		query=self.get_argument('q','')
-		results=self.search(query)	
-		self.render('templates/index.html',query=query,results=results)
+  
+  def get(self):
+    query=self.get_argument('q','')
+    results=self.search(query)	
+    self.render('templates/index.html',query=query,results=results)
 
-        def get_entities(self,query):
-            client=solr.Solr(SOLR_URL)
+  def clean_results_summaries(self,results):
+    return [self.clean_result_summary(result) for result in results.results]
 
-            entitysearch=solr.SearchHandler(client,'/entities')
+  def clean_result_summary(self,result):
+    result['clean_summary']=feeds.clean_summary(result['summary'])
+    return result
 
-            entity_results=entitysearch(query)
+  def get_top_result(self,query):
+    client=solr.Solr(SOLR_URL) 
+    topsearch=solr.SearchHandler(client,'/searchtop')
+    topsearch_results=topsearch(query)
+    if len(topsearch_results.results)>0:
+      return self.clean_result_summary(topsearch_results.results[0])
+    else:
+      return None
 
-            entities=[]
+  def get_entities(self,query):
+    client=solr.Solr(SOLR_URL) 
+    entitysearch=solr.SearchHandler(client,'/entities')
+    entity_results=entitysearch(query)
+    return []
+    #return [key for key,value in entity_results.facet_counts['facet_fields']['entity'].iteritems()]
+    
+    #entities=[]
 
-            if 'entity' in entity_results.facet_counts['facet_fields']:
-              for key,value in entity_results.facet_counts['facet_fields']['entity'].iteritems():
-                entities.append(key)
+    #if 'entity' in entity_results.facet_counts['facet_fields']:
+    #  for key,value in entity_results.facet_counts['facet_fields']['entity'].iteritems():
+    #    entities.append(key)
 
-            return entities
+    #return entities
 
-	def search(self,query):
-		client=solr.Solr(SOLR_URL)
-                            
-                searchleft=solr.SearchHandler(client,'/searchleft')
-                searchright=solr.SearchHandler(client,'/searchright')
-
-		# get left wing results
-                left_results=searchleft(query)
-                
-                # get right wing results
-                right_results=searchright(query)
-
-                top_result=None
-              
-                if(len(right_results.results)>0):
-                  top_result=right_results.results[0]
-                else:
-                  if(len(left_results.results)>0):
-                    top_result=left_results.results[0]
-
-                facets=self.get_entities('*:*') 
-
-                result= {'left':left_results,'right':right_results,'facets':facets}
-                
-                if top_result is not None:
-                  result['top']=top_result
-
-                return result
-
-def merge_highlights(results,fieldnames):
-	# merge highlights
-        for doc in results.results:
-                  docid=doc['id']
-                  # find highlights
-                  if docid in results.highlighting:
-                          for fieldname in fieldnames:
-                                  if fieldname in results.highlighting[docid]:
-                                          segments=results.highlighting[docid][fieldname]
-                                          highlight=''
-                                          for segment in segments:
-                                                  highlight=highlight+' '+segment
-                                          highlight=highlight.strip()
-                                          if len(highlight)>0 and not highlight[-1] in '.?!"\')':
-                                                  highlight=highlight+'...'
-                                          if len(highlight)>0 and not highlight[0] in 'abcdefghijklmnopqrstuvwxyz("\'':
-                                                  highlight='...'+highlight
-                                          doc[fieldname]=highlight
-
+  def search(self,query):
+    client=solr.Solr(SOLR_URL)
+    searchleft=solr.SearchHandler(client,'/searchleft')
+    searchright=solr.SearchHandler(client,'/searchright')
+    top_result=self.get_top_result(query)
+    # exclude top result from other searches (not working now for some reason)
+    fq=None
+    if top_result is not None:
+      fq="-id:"+top_result['id']
+    # get left wing results
+    if fq is not None:
+      left_results=self.clean_results_summaries(searchleft(query,fq=fq))
+    else:
+      left_results=self.clean_results_summaries(searchleft(query))
+    # get right wing results 
+    if fq is not None:
+      right_results=self.clean_results_summaries(searchright(query,fq=fq))
+    else:
+      right_results=self.clean_results_summaries(searchright(query))
+    # get top facets over all docs to show left-hand side topics
+    facets=self.get_entities('*:*') 
+    result= {'left':left_results,'right':right_results,'facets':facets}
+    if top_result is not None:
+      result['top']=top_result
+    return result
 
 def create_id_slug(s):
-	# strip out non-url and non-lucene friendly stuff...
-	
-	bad_chars="'+-&|!(){}[]^\"~*?:\\"
-	s=s.strip()
-	s=s.replace('http://','')
-	s=s.replace('https://','')
-	for c in bad_chars:
-		s=s.replace(c,'_')
-	
-	while '__' in s:
-		s=s.replace('__','_')
-	
-	return s	
+  # strip out non-url and non-lucene friendly stuff...
+  bad_chars="'+-&|!(){}[]^\"~*?:\\"
+  s=s.strip().replace('http://','').replace('https://','')
+  for c in bad_chars:
+    s=s.replace(c,'_')
+  while '__' in s:
+    s=s.replace('__','_')
+  return s	
 	
 class AutoSuggestHandler(tornado.web.RequestHandler):
 
   def get(self):
     prefix=self.get_argument('term','')
     client=solr.Solr(SOLR_URL)
-    # get matching terms
     terms_client=solr.SearchHandler(client,'/terms')
-
-    # return JSON
     results=terms_client(terms_regex=prefix+'.*')
-
-    
-    terms=results.terms['entity'].keys()
-
-    terms.sort()
-
-    json=[]
-    for term in terms:
-      json.append({'id':term,'label':term,'value':'"'+term+'"'})
+    json=[{'id':term,'label':term,'value':'"'+term+'"'} for term in sorted(results.terms['entity'].keys())]
     self.content_type = 'application/json'
     self.write(simplejson.dumps(json))
 
-class MltHandler(tornado.web.RequestHandler):
-
-
-	def mlt(self,itemid):
-		client=solr.Solr(SOLR_URL)
-	
-		results=client.select('id:'+itemid,mlt='true',mlt_fl='text',mlt_count=20,mlt_minwl=3,mlt_mintf=2,mlt_mindf=2)
-		
-		mlt={}		
-
-		if (results.results)>0:
-			mlt['item']=results.results[0]
-		else:
-			mlt['item']={'title':itemid,'wing':'NA'}
-
-		if itemid in results.moreLikeThis:
-			tmp = results.moreLikeThis[itemid]
-			# get right wing
-			mlt['right']=filter(lambda x:x['wing']=='right',tmp)
-			# get left wing
-			mlt['left']=filter(lambda x:x['wing']=='left',tmp)
-
-		return mlt
-
-
-	def get(self):
-			
-		itemid=self.get_argument('id')
-		itemid=create_id_slug(itemid)
-		# find more like this...
-		
-		results=self.mlt(itemid)
-		
-		self.render('templates/mlt.html',itemid=itemid,item=results['item'],results=results)
-
-
-
 application = tornado.web.Application([
-                                        (r"/", SearchHandler),
-                                        (r"/search",SearchHandler),
-                                        (r"/mlt",MltHandler),
-                                        (r"/autosuggest",AutoSuggestHandler)],
-                                        static_path=os.path.join(os.path.dirname(__file__),"static")
-                                        )
+              (r"/", SearchHandler),
+              (r"/search",SearchHandler),
+              (r"/autosuggest",AutoSuggestHandler)],
+              static_path=os.path.join(os.path.dirname(__file__),"static")
+              )
 
 if __name__ == "__main__":
-	application.listen(8888)
-	tornado.ioloop.IOLoop.instance().start()
+  application.listen(8888)
+  tornado.ioloop.IOLoop.instance().start()
